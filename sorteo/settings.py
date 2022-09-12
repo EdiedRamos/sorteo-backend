38b6eec6683dbf024de08d1d622ee514dd8723c4
EdiedRamos <edieisa22@gmail.com>
2022-09-11 23:13:40 -0500

    test

--- sorteo/settings.py
+++ sorteo/settings.py
@@ -135,6 +135,6 @@ STATIC_URL = '/static/'
 os.makedirs(STATIC_TMP, exist_ok=True)
 os.makedirs(STATIC_ROOT, exist_ok=True)
 
-STATICFILES_DIRS = [
+STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static')
-]
+)
