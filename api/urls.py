a683131b520ee2e25ec1a199c325719df3d22af1
EdiedRamos <edieisa22@gmail.com>
2022-09-12 00:00:47 -0500

    ulrs updated

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import CompanyView
 
 urlpatterns = [
-  path('companies/', CompanyView.as_view(), name='companies'),
+  path('companies', CompanyView.as_view(), name='companies'),
   path('companies/<int:nit>', CompanyView.as_view(), name='companiesNIT')
 ]
